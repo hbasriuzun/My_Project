15c568bd4d66fd785b5f89dc9892f12d38d5373c
Hasan Basri Uzun <basri.uzun@std.yildiz.edu.tr>
2023-09-22 17:21:01 +0300

    solve quesiton

--- a.cpp
+++ a.cpp
@@ -1,16 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <string>
+//Brace your keyboard
+//inzva community built algoleague for every algorithm enthusiast hungry for self-improvement and friendly competition. Have fun and good luck!
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+int main() {
+	int n,e,fr,sc;
+	cin >> n >> e;
+	vector<vector<int>> nd(n);
+	for(int i=0; i<e; i++){
+	    cin >> fr >> sc;
+	    nd[fr].push_back(sc);
+	    nd[sc].push_back(fr);
+	}
+	for(int i=0; i<n; i++){
+	    cout << nd[i].size() << " ";
+	}
+	return 0;
 }
